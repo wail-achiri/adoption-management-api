c6f102edddba7ca398d7c5eeab5d912f7ad95479
wail-achiri <wail.elachiri@gmail.com>
2026-01-29 00:10:00 +0100

    feature: configure dependency injection and database connection

--- AnimalShelter.Api/Program.cs
+++ AnimalShelter.Api/Program.cs
@@ -1,3 +1,12 @@
+using System.Text.Json.Serialization;
+using AnimalShelter.Api.Data;
+using AnimalShelter.Api.Models.Enums;
+using AnimalShelter.Api.Repositories;
+using AnimalShelter.Api.Repositories.Interfaces;
+using AnimalShelter.Api.Services;
+using AnimalShelter.Api.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,14 +17,40 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = builder.Configuration.GetConnectionString("Connection");
+builder.Services.AddDbContext<AnimalShelterDbContext>(opt =>
+    opt.UseNpgsql(
+        connectionString,
+        o =>
+        {
+            o.MapEnum<AdoptionState>("adoption_state");
+            o.MapEnum<AnimalState>("animal_state");
+            o.MapEnum<AnimalType>("animal_type");
+            o.MapEnum<UserRol>("user_rol");
+            o.MapEnum<UserState>("user_state");
+        }));
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(
+            new JsonStringEnumConverter()
+        );
+    });
+
+builder.Services.AddScoped<IAnimalService, AnimalService>();
+builder.Services.AddScoped<IAnimalRepository, AnimalRepository>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment()) 
 {
     //app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
