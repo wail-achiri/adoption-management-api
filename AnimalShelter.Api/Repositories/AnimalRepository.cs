99fc72cbe9cdad1e95826cf547f2af7d233c11f8
wail-achiri <wail.elachiri@gmail.com>
2026-01-29 00:09:35 +0100

    feature: implement animal repository pattern

---
+++ AnimalShelter.Api/Repositories/AnimalRepository.cs
@@ -0,0 +1,22 @@
+﻿using AnimalShelter.Api.Data;
+using AnimalShelter.Api.Models;
+using AnimalShelter.Api.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelter.Api.Repositories
+{
+    public class AnimalRepository : IAnimalRepository
+    {
+        private readonly AnimalShelterDbContext _dbcontext; 
+
+        public AnimalRepository(AnimalShelterDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task<List<AnimalModel>> GetAllAnimalsAsync()
+        {
+            return await _dbcontext.Animals.ToListAsync();
+        }
+    }
+}
---
+++ AnimalShelter.Api/Repositories/Interfaces/IAnimalRepository.cs
@@ -0,0 +1,9 @@
+﻿using AnimalShelter.Api.Models;
+
+namespace AnimalShelter.Api.Repositories.Interfaces
+{
+    public interface IAnimalRepository
+    {
+        Task<List<AnimalModel>> GetAllAnimalsAsync();
+    }
+}
