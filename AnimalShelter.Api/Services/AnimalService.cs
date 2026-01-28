17fbecf27a04d639c887e365af5d2ade9969ed4e
wail-achiri <wail.elachiri@gmail.com>
2026-01-29 00:09:57 +0100

    feature: implement animal service layer

---
+++ AnimalShelter.Api/Services/AnimalService.cs
@@ -0,0 +1,32 @@
+﻿using AnimalShelter.Api.Models;
+using AnimalShelter.Api.Repositories.Interfaces;
+using AnimalShelter.Api.Services.Interfaces;
+
+namespace AnimalShelter.Api.Services
+{
+    public class AnimalService : IAnimalService
+    {
+
+        private IAnimalRepository _animalRepository; 
+
+        public AnimalService(IAnimalRepository animalRepository) { 
+            _animalRepository = animalRepository;
+        }
+
+        public async Task<IEnumerable<AnimalDto>> GetAllAnimalsAsync()
+        {
+            var animals = await _animalRepository.GetAllAnimalsAsync();
+            return animals.Select(animal => new AnimalDto
+            {
+                Id = animal.Id,
+                Name = animal.Name,
+                Age = animal.Age,
+                Breed = animal.Breed,
+                Type = animal.Type.ToString(),
+                State = animal.State.ToString()
+            });
+        }
+
+
+    }
+}
---
+++ AnimalShelter.Api/Services/Interfaces/IAnimalService.cs
@@ -0,0 +1,9 @@
+﻿using AnimalShelter.Api.Models;
+
+namespace AnimalShelter.Api.Services.Interfaces
+{
+    public interface IAnimalService
+    {
+        Task<IEnumerable<AnimalDto>> GetAllAnimalsAsync();
+    }
+}
