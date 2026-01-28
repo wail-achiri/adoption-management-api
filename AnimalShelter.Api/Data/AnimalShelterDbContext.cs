c173a7a4d821f5cc6032ea742b4ae9197826a230
wail-achiri <wail.elachiri@gmail.com>
2026-01-29 00:09:34 +0100

    feature: add database context and migrations

---
+++ AnimalShelter.Api/Data/AnimalShelterDbContext.cs
@@ -0,0 +1,76 @@
+﻿using AnimalShelter.Api.Models;
+using AnimalShelter.Api.Models.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelter.Api.Data
+{
+    public class AnimalShelterDbContext : DbContext
+    {
+        public AnimalShelterDbContext(DbContextOptions <AnimalShelterDbContext> options) : base(options)
+        {
+            //CONFIGURAMOS NUESTRO DBSET
+        }
+
+
+        //ENTIDADES EN EF
+        public DbSet<AnimalModel> Animals { get; set; }
+        public DbSet<AdoptionModel> Adoptions { get; set; }
+        public DbSet<UserModel> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.HasPostgresEnum<AdoptionState>();
+            modelBuilder.HasPostgresEnum<AnimalState>();
+            modelBuilder.HasPostgresEnum<AnimalType>();
+            modelBuilder.HasPostgresEnum<UserRol>();
+            modelBuilder.HasPostgresEnum<UserState>();
+
+
+            // Configuración de la tabla Adoptions con múltiples FK hacia Users
+
+            modelBuilder.Entity<AdoptionModel>(entity =>
+            {
+                // ENUM
+                entity.Property(a => a.State)
+                    .HasColumnType("adoption_state");
+
+                // RELACIONES
+                entity.HasOne(a => a.Volunteer)
+                    .WithMany(u => u.VolunteerAdoptions)
+                    .HasForeignKey(a => a.VolunteerId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(a => a.Adopter)
+                    .WithMany(u => u.AdopterAdoptions)
+                    .HasForeignKey(a => a.AdopterId)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(a => a.Animal)
+                    .WithMany(an => an.Adoptions)
+                    .HasForeignKey(a => a.AnimalId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<AnimalModel>(entity =>
+            {
+                entity.Property(a => a.Type)
+                    .HasColumnType("animal_type");
+
+                entity.Property(a => a.State)
+                    .HasColumnType("animal_state");
+            });
+
+
+            modelBuilder.Entity<UserModel>(entity =>
+            {
+                entity.Property(u => u.Rol)
+                    .HasColumnType("user_rol");
+
+                entity.Property(u => u.State)
+                    .HasColumnType("user_state");
+            });
+        }
+    }
+}
---
+++ AnimalShelter.Api/Migrations/20260124183237_InitialWithEnums.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AnimalShelter.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialWithEnums : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:adoption_state", "rejected,inprocess,completed,canceled")
+                .Annotation("Npgsql:Enum:animal_state", "waiting,in_adoption,adopted,not_available")
+                .Annotation("Npgsql:Enum:animal_type", "dog,cat")
+                .Annotation("Npgsql:Enum:user_rol", "volunteer,adopter,admin")
+                .Annotation("Npgsql:Enum:user_state", "active,inactive,suspended");
+
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Age = table.Column<int>(type: "integer", nullable: true),
+                    Breed = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<int>(type: "animal_type", nullable: false),
+                    State = table.Column<int>(type: "animal_state", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Lastname = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Rol = table.Column<int>(type: "user_rol", nullable: false),
+                    State = table.Column<int>(type: "user_state", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Adoptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AnimalId = table.Column<int>(type: "integer", nullable: false),
+                    VolunteerId = table.Column<int>(type: "integer", nullable: false),
+                    AdopterId = table.Column<int>(type: "integer", nullable: false),
+                    AdoptionDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    State = table.Column<int>(type: "adoption_state", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adoptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Adoptions_Animals_AnimalId",
+                        column: x => x.AnimalId,
+                        principalTable: "Animals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Adoptions_Users_AdopterId",
+                        column: x => x.AdopterId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Adoptions_Users_VolunteerId",
+                        column: x => x.VolunteerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adoptions_AdopterId",
+                table: "Adoptions",
+                column: "AdopterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adoptions_AnimalId",
+                table: "Adoptions",
+                column: "AnimalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adoptions_VolunteerId",
+                table: "Adoptions",
+                column: "VolunteerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Adoptions");
+
+            migrationBuilder.DropTable(
+                name: "Animals");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
