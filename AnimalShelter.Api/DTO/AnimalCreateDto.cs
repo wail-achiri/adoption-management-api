1e7e0b720cffe5e27f83a81e38a1d193ca7b54dd
wail-achiri <wail.elachiri@gmail.com>
2026-02-09 21:37:52 +0100

    feature: add animal create and update DTOs

---
+++ AnimalShelter.Api/DTO/AnimalCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using AnimalShelter.Api.Models.Enums;
+
+namespace AnimalShelter.Api.Models
+{
+    public class AnimalCreateDto
+    {
+        public string Name { get; set; }
+        public int? Age { get; set; }
+        public string Breed { get; set; }
+        public string Type { get; set; }
+        public string State { get; set; }
+
+    }
+
+}
