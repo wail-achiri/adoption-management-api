4c9227501fc6aefcd3f18c9cdc2f812ddf896823
wail-achiri <wail.elachiri@gmail.com>
2026-02-09 21:38:39 +0100

    feature: update dependency injection configuration

--- AnimalShelter.Api/Program.cs
+++ AnimalShelter.Api/Program.cs
@@ -5,7 +5,9 @@ using AnimalShelter.Api.Repositories;
 using AnimalShelter.Api.Repositories.Interfaces;
 using AnimalShelter.Api.Services;
 using AnimalShelter.Api.Services.Interfaces;
+using AnimalShelter.Api.Services; // ensure AdopterService is available
 using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,14 +23,7 @@ var connectionString = builder.Configuration.GetConnectionString("Connection");
 builder.Services.AddDbContext<AnimalShelterDbContext>(opt =>
     opt.UseNpgsql(
         connectionString,
-        o =>
-        {
-            o.MapEnum<AdoptionState>("adoption_state");
-            o.MapEnum<AnimalState>("animal_state");
-            o.MapEnum<AnimalType>("animal_type");
-            o.MapEnum<UserRol>("user_rol");
-            o.MapEnum<UserState>("user_state");
-        }));
+        o => o.MapPostgresEnums()));
 
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
@@ -40,6 +35,9 @@ builder.Services.AddControllers()
 
 builder.Services.AddScoped<IAnimalService, AnimalService>();
 builder.Services.AddScoped<IAnimalRepository, AnimalRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IAdopterService, AdopterService>();
 
 
 var app = builder.Build();
